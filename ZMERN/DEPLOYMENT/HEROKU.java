856d0f7d85e7eb2b39943756df7b5b5813f89b32
prasann <prasannpatil98@gmail.com>
2021-07-13 14:17:43 +0530

    changes

--- ZMERN/DEPLOYMENT/HEROKU.java
+++ ZMERN/DEPLOYMENT/HEROKU.java
@@ -1,12 +1,25 @@
 
 //CREDENTIALS HEROKU
 /*
-GMAIL:prasannpatil98@gmail.com
-PASS:Wxyz12345@
+
 */
 
-//DEPLOY 
+//DEPLOY BACKEND ON HEROKU
 /*
-DEPLOY TO HEROKU 
-https://www.youtube.com/watch?v=aibtHnbeuio&t=0s : 1:10:58 TO 1:21:15
-*/
+https://www.youtube.com/watch?v=aibtHnbeuio&t=0s : 1:10:58 TO 1:19:00
+                                                   NOTE:THE VIDEO DOESNT HAVE BIN/WWW AND ONLY APP.JS SO PORT IS UDED IN OLLY APP.JS
+                                                        OUR PROJECT HAS BIN/WWW AS WELL AS APP.JS SO WE NEED TO GIVE VARIABLES TO PREVENT CLASH
+HEROKU CREDENTIALS
+GMAIL:prasannpatil98@gmail.com
+PASS:Wxyz12345@
+ */
+
+ //DEPLOY FRONTEND ON NETLIFY
+ /*
+ https://www.youtube.com/watch?v=aibtHnbeuio&t=0s :  1:19:00 to end 
+ 
+
+NETLIFY CREDENTIALS
+GMAIL:prasannpatil98@gmail.com
+PASS:Prasann12345@
+ */
--- server/env.java
+++ server/env.java
@@ -2,10 +2,14 @@
 //SET ENV VAR
 >export/set VAR_NAME=value  //linux/windows  
 OR
-.env               -->gitignore .env because to some variables must be kept private like mongodb url              
-VARNAME = value
-.env.example       -->gitignore not .env.exampe because it will show user the variables to include 
-VARNAME = valueHint
+.env ;.env.example -->gitignore .env because to some variables must be kept private like mongodb url  ;dont gitignore  .env.exampe because it will show user the variables to include             
+VARNAME = value    -->make sure varname does not clash with other varname because if they clash app crashes
+                     PORT = 3000                     -->bin/www.js uses process.env.PORT ||3000   which is port for server
+                     MONGODB_CONNECTION_PORT = 5000  -->app.js  uses process.env.MONGODB_CONNECTION_PORT||5000  which is port for mongodb connection to server
+                                                       so using different variable prevent clashing of ports
+      
+      
+
 
 //USE ENV VAR
 var dotenv = require('dotenv');              //sudo npm install (while project creation)
